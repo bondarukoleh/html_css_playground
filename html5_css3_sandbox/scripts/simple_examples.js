7aa064db680f3067b31f2f4cbf7ed69a9f631d60
Oleh.Bondaruk <bondaruk9000@gmail.com>
2018-09-27 01:53:47 +0300

    trying menu

--- html5_css3_sandbox/scripts/simple_examples.js
+++ html5_css3_sandbox/scripts/simple_examples.js
@@ -1,6 +1,16 @@
 const modernizerResults = document.querySelector('#modernizerResults')
-if(Modernizr.canvas){
-  modernizerResults.innerText += 'Yes, you can.'
+if (Modernizr.canvas) {
+  modernizerResults.innerText += 'Yes, we can.'
 } else {
-  modernizerResults.innerText += 'No, you cann\'t.'
+  modernizerResults.innerText += 'No, we cann\'t.'
+}
+
+/* Rotation */
+const picture = document.querySelector('#puh')
+function rotateLeft() {
+  const rotateValue = parseInt(picture.style.transform) + 90
+  picture.style.transform = `rotate(${rotateValue}deg)`
+}
+function rotateRight() {
+  picture.classList.toggle('rotateRight')
 }
