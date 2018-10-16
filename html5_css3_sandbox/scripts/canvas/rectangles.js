cce1b7a48aca2304238ab3d913024b36a6aa7eb2
Oleh Bondaruk <olebon@ciklum.com>
2018-10-16 12:37:15 +0300

    smile added

--- html5_css3_sandbox/scripts/canvas/rectangles.js
+++ html5_css3_sandbox/scripts/canvas/rectangles.js
@@ -1,6 +1,6 @@
+/*--------------- rect ------------------------*/
 const rectangl1 = document.querySelector('#rectangl1')
 const rec1Context = rectangl1.getContext('2d')
-
 rec1Context.fillStyle='rgb(200,0,0)';
 rec1Context.fillRect(10, 10, 150, 110);
 rec1Context.fillStyle='rgb(0, 0, 200, 0.5)';
@@ -9,7 +9,7 @@ rec1Context.clearRect(40, 40, 50, 50);
 rec1Context.lineWidth=3;
 rec1Context.strokeRect(35, 35, 60, 60);
 
-
+/*--------------- rombs ------------------------*/
 const line1 = document.querySelector('#line1')
 const line1Context = line1.getContext('2d')
 function drawRomb ({context, lineWidth}) {
@@ -28,3 +28,17 @@ drawRomb({context: line1Context, lineWidth: 2});
 line1Context.save();
 line1Context.translate(20, 5);
 drawRomb({context: line1Context, lineWidth: 4});
+
+/*--------------- circle smile ------------------------*/
+const circles = document.querySelector('#cirles')
+const circlesContext = circles.getContext('2d')
+circlesContext.beginPath()
+circlesContext.arc(35, 35, 30, 0, Math.PI*2)
+circlesContext.moveTo(25,  25)
+circlesContext.arc(25, 25, 5, 0, Math.PI*2)
+circlesContext.moveTo(45,  25)
+circlesContext.arc(45, 25, 5, 0, Math.PI*2)
+circlesContext.moveTo(35,  35)
+circlesContext.arc(35, 35, 25, 0, Math.PI)
+circlesContext.closePath()
+circlesContext.stroke()
